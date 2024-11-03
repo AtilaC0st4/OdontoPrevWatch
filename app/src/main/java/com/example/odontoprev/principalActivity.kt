3afb22eff5e468be3da45de6c6b25a2068da8a90
Átila <atilarebolo@gmail.com>
2024-11-02 22:48:27 -0300

    implmentando o menu do app

--- app/src/main/java/com/example/odontoprev/principalActivity.kt
+++ app/src/main/java/com/example/odontoprev/principalActivity.kt
@@ -3,18 +3,42 @@ package com.example.odontoprev
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.odontoprev.databinding.ActivityPrincipalBinding
 
-class principalActivity : AppCompatActivity() {
+class PrincipalActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityPrincipalBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_principal)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        replaceFragment(homeFragment()) // Corrigido para Kotlin
+
+        binding.navigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    replaceFragment(homeFragment())
+                }
+                R.id.relogio -> {
+                    replaceFragment(relogioFragment())
+                }
+                R.id.perfil -> {
+                    replaceFragment(perfilFragment())
+                }
+            }
+            true
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
